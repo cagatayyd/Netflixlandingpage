d9585e188304da8356a21cbcd2ad3cd9336a6f30
Cagatay <cqty_57@hotmail.com>
2022-08-26 15:41:57 +0300

    first

---
+++ app.js
@@ -0,0 +1,18 @@
+function dropDown1(){
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+function dropDown2(){
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+function dropDown3(){
+    document.getElementById("myDropdown3").classList.toggle("show");
+}
+function dropDown4(){
+    document.getElementById("myDropdown4").classList.toggle("show");
+}
+function dropDown5(){
+    document.getElementById("myDropdown5").classList.toggle("show");
+}
+function dropDown6(){
+    document.getElementById("myDropdown6").classList.toggle("show");
+}
